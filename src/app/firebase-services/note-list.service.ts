521f7cedb779fcc7d5bb223202271cf89b3e1660
Martin Unger <martin-unger2@live.at>
2023-10-23 19:53:13 +0200

    collection() + doc()

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore , collection, collectionData} from '@angular/fire/firestore';
+import { doc } from '@firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  trashNotes: Note[] = [];
+  normNotes: Note[] = [];
+
+  items$;
+  items;
+
+  firestore: Firestore = inject(Firestore);
+
+  constructor() { 
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe( (list) => {
+    	list.forEach(element => {
+        console.log(element);
+      });
+    });
+    this.items.unsubscribe();
+  }
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+  getSingleDocRef(colId:string, docId:string) {
+    return doc(collection(this.firestore, colId), docId);
+  }
 }
